915d61d5d51e1188df5b8357f0d58fa60e485202
alizeba11323 <alizeba11323@gmail.com>
2023-12-05 12:07:43 +0530

    logo image changed

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import React from "react";
-import LogoNew from "../../public/images/logo.png";
+import LogoNew from "../../public/images/logo.jpeg";
 import { useRouter } from "next/navigation";
 function Logo() {
   const router = useRouter();
@@ -10,7 +10,7 @@ function Logo() {
       className="hidden md:block cursor-pointer"
       onClick={() => router.push("/")}
     >
-      <Image alt="Logo" src={LogoNew} width="100" height="100" />
+      <Image alt="Logo" src={LogoNew} width="50" height="50" />
     </div>
   );
 }
